025b7ff4e0894f9e0f2f9c9a1727b521e18fcebd
Shani Aharon <rivena14798@gmail.com>
2024-05-01 05:07:11 +0300

    add config

---
+++ config/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+var config
+
+if (process.env.NODE_ENV === 'production') {
+  config = require('./prod')
+} else {
+
+  config = require('./dev')
+}
+config.isGuestMode = true
+
+module.exports = config
